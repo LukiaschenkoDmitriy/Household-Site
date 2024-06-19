ff6d2da300eb55236bd5271e97ab910af15f482d
DLukiashchenko <lukiaschenkoff@gmail.com>
2024-06-19 03:15:36 +0200

    Slider

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,13 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+
+import '@splidejs/splide/css';
+import Splide from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+new Splide( '.splide', {
+    arrows: false,
+    speed: 2000,
+    type: "loop"
+} ).mount( { AutoScroll } );
